10da1ac6577c8dafced372a6ba4664db35c5a026
Andrew <andrew.colinet@alum.utoronto.ca>
2024-03-18 11:19:09 -0400

    Modified autoscaling group specifications.

--- autoscaling.tf
+++ autoscaling.tf
@@ -1,60 +1,10 @@
 # create autoscaling group
 
-# Availability Zone 1
-#
-#resource "aws_autoscaling_group" "autoscaling_group_availability_zone_1" {
-#  desired_capacity    = 1  # desired number of instances at a given time
-#  max_size            = 1  # maximum number of instances group should have
-#  min_size            = 1  # minimum number of instances group should have
-#  vpc_zone_identifier = [aws_subnet.private_subnet_1.id]
-#  target_group_arns   = [aws_lb_target_group.prod_vpclb_target_group.arn]
-#
-#  launch_template {
-#    id      = aws_launch_template.web_lc.id
-#    version = aws_launch_template.web_lc.latest_version
-#  }
-#}
-#
-#
-#resource "aws_launch_template" "web_lc" {
-#  name              = "Web-Server"
-#  image_id          = "ami-0e731c8a588258d0d"
-#  instance_type     = "t2.micro"
-#
-#  user_data         = filebase64("./userdata.sh")
-#
-#  block_device_mappings {
-#    device_name = "/dev/sdf"
-#
-#    ebs {
-#      volume_size = 5
-#    }
-#  }
-#
-#  network_interfaces {
-#    associate_public_ip_address = false
-#    subnet_id                   = aws_subnet.private_subnet_1.id
-#    security_groups             = [aws_security_group.appsg.id]
-#  }
-#
-#  tag_specifications {
-#    resource_type = "instance"
-#
-#    tags = {
-#      Name = "Launch template availability zone 2"
-#    }
-#  }
-#}
-
-
-
-# Availability Zone 2
-
 #  autoscaling group
 resource "aws_autoscaling_group" "autoscaling_group_availability_zone_2" {
-  desired_capacity    = 1  # desired number of instances at a given time
-  max_size            = 1  # maximum number of instances group should have
-  min_size            = 1  # minimum number of instances group should have
+  desired_capacity    = 2  # desired number of instances at a given time
+  max_size            = 5  # maximum number of instances group should have
+  min_size            = 2  # minimum number of instances group should have
   vpc_zone_identifier = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_3.id]
   target_group_arns   = [aws_lb_target_group.prod_vpclb_target_group.arn]
 
