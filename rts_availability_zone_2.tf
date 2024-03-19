c601c8133bb9f8b7e76d04b56958d64e29f3d129
Javaria Nauman <14944845+jazznauman@users.noreply.github.com>
2024-03-18 20:45:58 -0400

    Updated AZ#1 info to this

--- rts_availability_zone_2.tf
+++ rts_availability_zone_2.tf
@@ -1,4 +1,31 @@
-# Presentation Layer Public Route Table
+# Presentation Layer Public Route Table For Public Subnet 1
+resource "aws_route_table" "publicroute1" {
+  vpc_id = aws_vpc.prod_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.prod_vpcigw.id
+  }
+
+  tags = {
+    Name = "PublicRouteTable1"
+  }
+}
+
+
+#Create Public Route table1 association
+
+
+resource "aws_route_table_association" "publicrta1" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.publicroute1.id
+}
+
+
+
+
+
+# Presentation Layer Public Route Table For Public Subnet 2
 
 resource "aws_route_table" "pres_layer_rt" {
   vpc_id = aws_vpc.prod_vpc.id
@@ -10,15 +37,18 @@ resource "aws_route_table" "pres_layer_rt" {
 }
 
 
-# create public route table association
+# create public route table association for public subnet 2
 resource "aws_route_table_association" "pres_layer_rta" {
   subnet_id      = aws_subnet.public_subnet_2.id
   route_table_id = aws_route_table.pres_layer_rt.id
 }
+
+
+
 ###########################################################################################
 
 
-# Business Logic Layer Public Route Table
+# Business Logic Layer Private Route Table For Private Subnet 3
 resource "aws_route_table" "bus_log_layer_rt" {
   vpc_id = aws_vpc.prod_vpc.id
 
@@ -29,21 +59,78 @@ resource "aws_route_table" "bus_log_layer_rt" {
 }
 
 
-# create public route table association
+# create private route table association
 resource "aws_route_table_association" "bus_log_layer_rta" {
   subnet_id      = aws_subnet.private_subnet_3.id
   route_table_id = aws_route_table.bus_log_layer_rt.id
 }
+
+
+# Business Logic Layer Private Route Table For Private Subnet 1
+
+resource "aws_route_table" "pvtroute1" {
+  vpc_id = aws_vpc.prod_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+
+  tags = {
+    Name = "PrivateRouteTable1"
+  }
+}
+
+#Create Private Route table1 association
+
+
+resource "aws_route_table_association" "pvtrta1" {
+  subnet_id      = aws_subnet.private1.id
+  route_table_id = aws_route_table.pvtroute1.id
+}
+
+
 ###########################################################################################
 
 
-# Database Storage Layer Private Route Table
+
+
+#Database Storage Layer Private Route Table for Private Subnet 2
+resource "aws_route_table" "pvtroute2" {
+  vpc_id = aws_vpc.prod_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+
+  tags = {
+    Name = "PrivateRouteTable2"
+  }
+}
+
+
+#Create Private Route table2 association for Private Subnet 2
+
+
+resource "aws_route_table_association" "pvtrta2" {
+  subnet_id      = aws_subnet.private2.id
+  route_table_id = aws_route_table.pvtroute2.id
+}
+
+
+
+
+
+# Database Storage Layer Private Route Table for Private Subnet 4
 resource "aws_route_table" "data_stor_layer_rt" {
   vpc_id = aws_vpc.prod_vpc.id
 }
 
 
-# create private route table association
+# create private route table association for Private Subnet 4
 resource "aws_route_table_association" "data_stor_layer_rta" {
   subnet_id      = aws_subnet.private_subnet_4.id
   route_table_id = aws_route_table.data_stor_layer_rt.id
